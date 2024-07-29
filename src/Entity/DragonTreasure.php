44a7d1029b564b0db7e1f8bff3782eacdc90e75a
Farna Chilashvili <farnnn.ff@gmail.com>
2024-07-29 19:18:58 +0400

    Add api platform validations.

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
@@ -11,12 +12,15 @@ use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\DragonTreasureRepository;
 use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
+use function Symfony\Component\String\u;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
@@ -29,14 +33,19 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
         new Put(),
         new Patch()
     ],
+    formats: [
+        'jsonld',
+        'csv' => 'text/csv'
+    ],
     normalizationContext: [
         'groups' => ['treasure:read']
     ],
     denormalizationContext: [
         'groups' => ['treasure:write']
     ],
-    paginationItemsPerPage: 10,
+    paginationItemsPerPage: 10
 )]
+#[ApiFilter(PropertyFilter::class)]
 class DragonTreasure
 {
     #[ORM\Id]
@@ -47,11 +56,14 @@ class DragonTreasure
     #[ORM\Column(length: 255)]
     #[Groups(['treasure:read', 'treasure:write'])]
     #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50, maxMessage: 'Use 50 or less characters')]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     #[Groups(['treasure:read', 'treasure:write'])]
     #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank()]
     private ?string $description = null;
 
     /**
@@ -59,11 +71,15 @@ class DragonTreasure
      */
     #[ORM\Column]
     #[Groups(['treasure:read', 'treasure:write'])]
-    private ?int $value = null;
+    #[ApiFilter(RangeFilter::class)]
+    #[Assert\GreaterThanOrEqual(0)]
+    private ?int $value = 0;
 
     #[ORM\Column]
     #[Groups(['treasure:read', 'treasure:write'])]
-    private ?int $coolFactor = null;
+    #[Assert\GreaterThanOrEqual(0)]
+    #[Assert\LessThanOrEqual(10)]
+    private ?int $coolFactor = 0;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $plunderedAt = null;
@@ -98,6 +114,11 @@ class DragonTreasure
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return u($this->description)->truncate(40, '...');
     }
 
     public function setDescription(string $description): static
