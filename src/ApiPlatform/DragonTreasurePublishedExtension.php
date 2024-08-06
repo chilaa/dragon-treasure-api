fae2c3f6a6defbd6938cbf5b676493b89b0959e9
Farna Chilashvili <farnnn.ff@gmail.com>
2024-08-06 12:45:25 +0400

    Create new querybuilder extension to return only published treasures for non amdin users.

---
+++ src/ApiPlatform/DragonTreasurePublishedExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\DragonTreasure;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class DragonTreasurePublishedExtension implements QueryCollectionExtensionInterface
+{
+
+    public function __construct(private Security $security)
+    {
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        Operation $operation = null,
+        array $context = []
+    ): void {
+        if (DragonTreasure::class !== $resourceClass) {
+            return;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+            ->setParameter('isPublished', true);
+    }
+}
--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -15,7 +15,13 @@ class DragonTreasureResourceTest extends ApiTestCase
 
     public function testCollectionOfTreasures(): void
     {
-        DragonTreasureFactory::createMany(5);
+        DragonTreasureFactory::createMany(5, [
+            'isPublished' => true
+        ]);
+
+        DragonTreasureFactory::createOne([
+            'isPublished' => false
+        ]);
 
         $json = $this->browser()
             ->get('/api/treasures')
