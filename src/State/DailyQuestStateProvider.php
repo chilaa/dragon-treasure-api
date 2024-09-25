33f9bbb51f4f134027ca800ec737a7daa79b348f
Farna Chilashvili <farnnn.ff@gmail.com>
2024-09-25 18:52:34 +0400

    Add fields to quest, add DailyQuestStateProvider

--- src/State/DailyQuestStateProvider.php
+++ src/State/DailyQuestStateProvider.php
@@ -5,14 +5,29 @@ namespace App\State;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\DailyQuest;
+use App\Enum\DailyQuestStatusEnum;
 
 class DailyQuestStateProvider implements ProviderInterface
 {
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        return [
-            new DailyQuest(1),
-            new DailyQuest(2),
-        ];
+        return $this->createQuests();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function createQuests(): array
+    {
+        $quests = [];
+        for ($i = 0; $i < 50; $i++) {
+            $quest = new DailyQuest(new \DateTimeImmutable(sprintf('- %d days', $i)));
+            $quest->questName = sprintf('Quest %d', $i);
+            $quest->description = sprintf('Description %d', $i);
+            $quest->difficultyLevel = $i % 10;
+            $quest->status = $i % 2 === 0 ? DailyQuestStatusEnum::ACTIVE : DailyQuestStatusEnum::COMPLETED;
+            $quests[$quest->getDayString()] = $quest;
+        }
+        return $quests;
     }
 }
