f0ed311cdd3be56a4ec215de8976ef2446b453d4
Farna Chilashvili <farnnn.ff@gmail.com>
2024-10-09 02:02:34 +0400

    Get DragonTreasure it's own dto and entity mapper.

---
+++ src/ApiResource/DragonTreasureApi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\ApiResource;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\DragonTreasure;
+use App\State\EntityClassDtoStateProcessor;
+use App\State\EntityToDtoStateProvider;
+
+#[ApiResource(
+    shortName: 'Treasure',
+    paginationItemsPerPage: 10,
+    provider: EntityToDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
+    stateOptions: new Options(entityClass: DragonTreasure::class)
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'username' => 'partial'
+])]
+class DragonTreasureApi
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    public ?string $id = null;
+
+    public ?string $name = null;
+
+    public ?string $description = null;
+
+    public ?int $value = 0;
+
+    public ?int $coolFactor = 0;
+
+    public ?UserApi $owner = null;
+
+    public ?string $shortDescription = null;
+
+    public ?string $plunderedAtAgo = null;
+
+    public ?bool $isMine = null;
+}
